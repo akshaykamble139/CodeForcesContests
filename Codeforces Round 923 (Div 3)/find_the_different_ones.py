7e55bd2d85292a9fbcf5b55d7e7dd6fbd5b01c9f
akshaykamble139 <kakshayiitk@gmail.com>
2024-02-08 16:39:39 +0530

    code changed

--- Codeforces Round 923 (Div 3)/find_the_different_ones.py
+++ Codeforces Round 923 (Div 3)/find_the_different_ones.py
@@ -4,19 +4,24 @@ while t > 0:
     t -= 1
     n = int(input())
     arr = [int(a) for a in input().split()]
+    p = [-1]*n
+    for i in range(1,n):
+        p[i] = p[i-1]
+        if arr[i] != arr[i-1]:
+            p[i] = i-1
+    # print(f"p = {p}")
     q = int(input())
-    dict = {}
-    for i in range(n):
-        if arr[i] in dict:
-            dict[arr[i]].append(i)
-        else:
-            dict[arr[i]] = [i]
-    print(dict)
+
     while q>0:
         q -= 1
         word = [int(a) for a in input().split()]
         l = word[0]-1
         r = word[1]-1
+
+        if p[r]<l:
+            print("-1 -1")
+        else:
+            print(p[r]+1, r+1)
 
 
 
